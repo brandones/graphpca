a7324ac916bb37d342b3d87db9bb05a4a53f0b08
Brandon Istenes <bistenes@socos.me>
2016-09-03 02:41:51 +0300

    Add naive implementation for comparison

--- graphpca/__init__.py
+++ graphpca/__init__.py
@@ -35,12 +35,11 @@ def reduce_graph(nx_graph, output_dim, add_supernode=False):
 
     and the corresponding eigenvectors are the same. Further, we only care
     about the top output_dim eigenpairs of pinv(L), which correspond to the
-    smallest
-    nonzero eigenvalues of L. Since calculating the smallest eigenpairs will
-    include the nullspace, we need to calculate the smallest
-    ``output_dim + nullity`` eigenpairs. Using `scipy.sparse.linalg.eigs` with
-    `which=SM` to calculate eigenpairs, we need to compute the nullity ahead of
-    time. To do this, we use the convenient fact that the nullity of L is equal
+    smallest nonzero eigenvalues of L. We use `scipy.sparse.linalg.eigs` with
+    `which=SM` to calculate eigenpairs, which includes zero eigenpairs.
+    Therefore in order to calculate the smallest nonzero eigenpairs we need
+    to calculate the smallest ``output_dim + nullity`` eigenpairs. We compute
+    the nullity using the convenient fact that the nullity of L is equal
     to the number of connected components in G.
 
     Parameters
@@ -100,7 +99,8 @@ def add_supernode_to_laplacian(L):
 
 def retry_eigendecomp(M, output_dim, tol=0, _attempt=0, **kwargs):
     try:
-        return scipy.sparse.linalg.eigs(M, output_dim, tol=tol, **kwargs)
+        # TODO: Use the more accurate "sigma" method
+        return scipy.sparse.linalg.eigsh(M, output_dim, tol=tol, **kwargs)
     except ArpackNoConvergence, e:
         if _attempt > 2:
           LOG.error('Eigendecomp did not converge. Bailing.')
@@ -111,6 +111,21 @@ def retry_eigendecomp(M, output_dim, tol=0, _attempt=0, **kwargs):
         new_tol = tol * 10
         LOG.info('Eigendecomp failed to converge, retrying with tolerance {}'.format(new_tol))
         return retry_eigendecomp(M, output_dim, tol=new_tol, _attempt=_attempt+1)
+
+
+def naive_reduce_graph(nx_graph, output_dim):
+    LOG.info('Calculating Laplacian L')
+    L = nx.laplacian_matrix(nx_graph).astype('f').todense()
+    Li = np.linalg.pinv(L)
+    LOG.info('Calculating largest eigenvalues of L-inverse & corresponding eigenvectors')
+    (E, U) = retry_eigendecomp(Li, output_dim)
+    LOG.info('Assembling PCA result')
+    # Assemble into the right structure
+    X = np.zeros((output_dim, len(nx_graph)))
+    sqrtE = np.sqrt(E)
+    for i in range(output_dim):
+        X[i, :] = sqrtE[i] * U[:, i]
+    return X
 
 
 def plot_2d(pca_output_2d, colormap_name='winter'):
--- test/test_graphpca.py
+++ test/test_graphpca.py
@@ -7,6 +7,7 @@ import unittest
 
 import networkx as nx
 import numpy as np
+import scipy.io
 
 import graphpca
 
@@ -33,3 +34,25 @@ class TestGraphPCA(unittest.TestCase):
         for i in range(5):
             max_val = max(abs(g_5[i]))
             self.assertGreater(max_val, 0.01)
+
+    def test_similar_output_to_naive(self):
+        mat = scipy.io.loadmat('bcspwr01.mat')
+        # I love the UFSMC (https://www.cise.ufl.edu/research/sparse/matrices/)
+        # but wow they really buried the matrix in this .mat
+        A = mat['Problem'][0][0][1].todense()
+        G = nx.from_numpy_matrix(A)
+        G2 = graphpca.reduce_graph(G, 2)
+        G2n = graphpca.naive_reduce_graph(G, 2)
+        print G2
+        print G2n
+        # Compare the fabs because eiganvalue parity is arbitrary
+        # Compare the row-swapped even though the eigenvalue order is not at
+        # all arbitrary because reduce_graph fails to accurately determine
+        # the smallest eigenvalues of L.
+        # TODO: improve eigenvalue calculation
+        difference_norm = min(np.linalg.norm(np.fabs(G2) - np.fabs(G2n)),
+                              np.linalg.norm(np.fabs(G2) - np.fabs(G2n[[1, 0]])))
+        print difference_norm
+        self.fail()
+        self.assertLess(difference_norm, 0.00001)
+
