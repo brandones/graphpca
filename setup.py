2cd3fe9b143a7c03b990b4bbc20f477e22b2189a
Brandon Istenes <bistenes@gmail.com>
2020-06-26 17:56:38 -0700

    Upgrade to 1.0.0 (and SemVer!)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*paths):
 setup(
   name = 'graphpca',
   packages = ['graphpca'],
-  version = '0.6',
+  version = '1.0.0',
   license = 'Apache License 2.0',
   description = 'Produces a low-dimensional representation of the input graph',
   long_description=(read('README.rst')),
