e614f66c4759e8d3a2c157146ab30ddf8edfaf51
Brandon Istenes <bistenes@gmail.com>
2024-05-28 23:35:34 -0400

    Version 1.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*paths):
 setup(
   name = 'graphpca',
   packages = ['graphpca'],
-  version = '1.1.0',
+  version = '1.1.1',
   license = 'Apache License 2.0',
   description = 'Produces a low-dimensional representation of the input graph',
   long_description=(read('README.rst')),
